2fc9063eaaff8dea6b4412e99bb3373ec494719e
yaotailin <yaotailin@gmail.com>
2020-03-28 20:54:54 +0800

    Ribbon负载规则默认替换

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderApplication80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderApplication80.java
@@ -1,14 +1,17 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.ribbonrule.RibbonRuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = RibbonRuleConfig.class)
 public class OrderApplication80 {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication80.class,args);
+        SpringApplication.run(OrderApplication80.class, args);
     }
 }
